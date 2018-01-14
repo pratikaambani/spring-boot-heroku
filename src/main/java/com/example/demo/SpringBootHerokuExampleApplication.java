14a9656b10d8ac46404cb6790fa53f59e4424877
Pratik Ambani <pratikaambani@gmail.com>
2018-01-14 10:47:03 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/SpringBootHerokuExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHerokuExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHerokuExampleApplication.class, args);
+	}
+}
